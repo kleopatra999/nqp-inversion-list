c743b6e2a9caf294624aca6152ffcaa9eed86940
Moritz Lenz <moritz@faui2k3.org>
2013-11-10 08:42:42 +0100

    make add_range accept inclusive ranges, more intuitive that way
    also fix the tests, and add some more

--- lib/InversionList.pm
+++ lib/InversionList.pm
@@ -57,9 +57,13 @@ class InversionList {
     }
 
     method add_range(int $from, int $to) {
+        self.add_range_excl($from, $to + 1);
+    }
+
+    method add_range_excl(int $from, int $to_excl) {
         my int $fpos := self._find_pos($from);
-        my int $tpos := self._find_pos($to + 1, $fpos);
-        nqp::splice(@!codepoints, [$from, $to], $fpos, $tpos - $fpos);
+        my int $tpos := self._find_pos($to_excl + 1, $fpos);
+        nqp::splice(@!codepoints, [$from, $to_excl], $fpos, $tpos - $fpos);
     }
 
     method merge(InversionList $other) {
@@ -67,7 +71,7 @@ class InversionList {
         my int $elems := nqp::elems(@other);
         my int $i := 0;
         while $i < $elems {
-            self.add_range(@other[$i], @other[$i + 1]);
+            self.add_range_excl(@other[$i], @other[$i + 1]);
             $i := $i + 2;
         }
     }
@@ -89,5 +93,8 @@ class InversionList {
         nqp::bindattr($new, InversionList, '@!codepoints',
             nqp::isnull(@codepoints) ?? nqp::list() !! @codepoints);
         $new;
+    }
+    method contains(int $codepoint) {
+        1 +& self._find_pos($codepoint + 1);
     }
 }
--- t/01-basic.t
+++ t/01-basic.t
@@ -1,12 +1,18 @@
 use InversionList;
 
-plan(4);
+plan(10);
 
 {
     my $il := InversionList.new();
     $il.add_range(10, 20);
-    ok($il._find_pos(1)  == 0, "1 is not in the IL");
-    ok($il._find_pos(10) == 1, "10 is in the IL");
-    ok($il._find_pos(20) == 1, "20 is in the IL");
-    ok($il._find_pos(30) == 2, "30 is not in the IL");
+    ok($il._find_pos(1)  == 0, "_find_pos(1)");
+    ok($il._find_pos(10) == 0, "_find_pos(10)");
+    ok($il._find_pos(20) == 1, "_find_pos(20)");
+    ok($il._find_pos(30) == 2, "_find_pos(30)");
+    ok(!$il.contains(0), 'does not contain 0');
+    ok(!$il.contains(9), 'does not contain 9');
+    ok( $il.contains(10), 'contains 10');
+    ok( $il.contains(15), 'contains 15');
+    ok( $il.contains(20), 'contains 20');
+    ok(!$il.contains(21), 'does not contain 21');
 }
